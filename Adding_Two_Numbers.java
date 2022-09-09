42549452f6494163be9cdd0e889ff269b3286437
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-09-09 21:06:57 +0530

    import java.util.Scanner; class twonum {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,x,y;         x=sc.nextInt();         y=sc.nextInt();         a=x+y;         System.out.println(a);     } }

---
+++ Adding_Two_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class twonum
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,x,y;
+        x=sc.nextInt();
+        y=sc.nextInt();
+        a=x+y;
+        System.out.println(a);
+    }
+}
