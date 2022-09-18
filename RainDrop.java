95ca723293ad31c441494985b6d96920e215ac34
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-09-18 21:41:29 +0530

    import java.util.Scanner; class RD {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n;         n=sc.nextInt();         if(n%3==0 & n%5==0)         {             System.out.println("PlingPlang");         }         else if(n%3==0 & n%7==0)         {             System.out.println("PlingPlong");         }         else if(n%5==0 & n%7==0)         {             System.out.println("PlangPlong");         }         else if(n%3==0)         {             System.out.println("Pling");         }         else if(n%5==0)         {             System.out.println("Plang");         }         else if(n%7==0)         {             System.out.println("Plong");         }         else         {             System.out.println(n);         }     } }

---
+++ RainDrop.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+class RD
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        if(n%3==0 & n%5==0)
+        {
+            System.out.println("PlingPlang");
+        }
+        else if(n%3==0 & n%7==0)
+        {
+            System.out.println("PlingPlong");
+        }
+        else if(n%5==0 & n%7==0)
+        {
+            System.out.println("PlangPlong");
+        }
+        else if(n%3==0)
+        {
+            System.out.println("Pling");
+        }
+        else if(n%5==0)
+        {
+            System.out.println("Plang");
+        }
+        else if(n%7==0)
+        {
+            System.out.println("Plong");
+        }
+        else
+        {
+            System.out.println(n);
+        }
+    }
+}
